16a40dbeaca18f22230e56b7483d1ce6a98bef13
Mohammad Fakih <mohammadfakih025@gmail.com>
2024-10-26 17:59:59 +0300

    made the tree appear in html

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 var n = prompt("Please enter size of tree");
 var i;
 var j;
-console.log(" ".repeat(n)+ " *");
+var tree="";
+tree+= " ".repeat(n)+ " *"+"\n";
 for(i=0;i<=n;i++){
+    tree+=" ".repeat(n-i)+"*"+"|".repeat((i*2)+1)+"*"+ "\n";
     console.log(" ".repeat(n-i)+"*"+"|".repeat((i*2)+1)+"*")
 }
+
+const para = document.createElement("pre");
+para.innerText = tree;
+document.body.appendChild(para);
